800ba9439856421acf04c2a048487e9ba99bbe73
Kai Tamkun <ktamkun@ucsc.edu>
2019-05-19 03:02:57 -0700

    Reorganized tests.cpp a bit

--- test/tests.cpp
+++ test/tests.cpp
@@ -14,13 +14,17 @@ namespace pingpong {
 namespace tests {
 	using namespace pingpong;
 
+	const serv_ptr serv = make_shared<server>("irc.subluminal.net");
+
 	void test_channel() {
 		channel chan("#programming");
 		channel user("NickServ");
 		cout << "chan.is_user(): " << chan.is_user() << endl;
 		cout << "user.is_user(): " << user.is_user() << endl;
 
-		serv_ptr serv = make_shared<server>("irc.subluminal.net");
+	}
+
+	void test_commands() {
 		user_command usercmd(serv, "some_user", "Some Name");
 		usercmd.send();
 
@@ -30,6 +34,6 @@ namespace tests {
 }
 
 int main(int, char **) {
-	tests::test_channel();
+	tests::test_commands();
 	return 0;
 }
