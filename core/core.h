40e2abb9346995546dfd6fb71332fe9b2fb9c1b4
Kai Tamkun <ktamkun@ucsc.edu>
2019-05-19 01:04:05 -0700

    Added forward declarations in core/core.h

---
+++ core/core.h
@@ -0,0 +1,10 @@
+#ifndef CORE_H_
+#define CORE_H_
+
+namespace pingpong {
+	class channel;
+	class server;
+	class irc;
+}
+
+#endif
--- core/irc.h
+++ core/irc.h
@@ -1,10 +1,15 @@
 #ifndef CORE_IRC_H_
 #define CORE_IRC_H_
 
+#include <vector>
+#include "core.h"
+
 namespace pingpong {
-	namespace irc {
-		constexpr int default_port = 6667;
-	}
+	class irc {
+		public:
+			static constexpr int default_port = 6667;
+			std::vector<pingpong::server> servers;
+	};
 }
 
 #endif
