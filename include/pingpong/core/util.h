b569f46ca52dfe7392b947c3b5b75e988753a59e
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-30 21:31:39 -0700

    Added a questionable irc-to-ansi color conversion function.

--- include/pingpong/core/util.h
+++ include/pingpong/core/util.h
@@ -1,6 +1,8 @@
 #ifndef PINGPONG_CORE_UTIL_H_
 #define PINGPONG_CORE_UTIL_H_
 
+#include <array>
+#include <deque>
 #include <memory>
 #include <chrono>
 #include <string>
@@ -13,6 +15,7 @@ namespace pingpong {
 		static constexpr const char *nick_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 			"-_[]{}\\`|";
 		static constexpr const char *flag_chars = "+-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		static std::array<std::string, 16> irc_colors;
 
 		static std::chrono::system_clock::duration now();
 
@@ -24,8 +27,32 @@ namespace pingpong {
 		/** Returns true if all the characters in a string are valid for nicknames. */
 		static bool is_valid_nick(const std::string &);
 
+		/** Trims whitespace from the end of a string. */
 		static std::string & rtrim(std::string &);
 		static std::string rtrim(const std::string &);
+
+		/** Converts mIRC colors to ANSI escapes. */
+		static std::string irc2ansi(std::string);
+
+		template <typename T, typename Iter, typename Pred>
+		static std::deque<T> take_while(Iter begin, Iter end, Pred predicate) {
+			std::deque<T> out {};
+			while (begin != end && predicate(*begin))
+				out.push_back(*begin);
+			return out;
+		}
+
+		template <typename Pred>
+		static std::string take_while(const std::string &str, Pred predicate) {
+			std::string out {};
+			for (char ch: str) {
+				if (!predicate(ch))
+					break;
+				out.push_back(ch);
+			}
+
+			return out;
+		}
 	};
 
 	template <typename T>
--- src/core/util.cpp
+++ src/core/util.cpp
@@ -1,8 +1,31 @@
 #include <cctype>
+#include <cstdlib>
+#include <sstream>
 
 #include "pingpong/core/util.h"
 
+#include "lib/formicine/ansi.h"
+
 namespace pingpong {
+	std::array<std::string, 16> util::irc_colors = {
+		"white",
+		"black",
+		"blue",
+		"green",
+		"red",
+		"brown",
+		"purple",
+		"orange",
+		"yellow",
+		"yeen",
+		"cyan",
+		"sky",
+		"blue",
+		"magenta",
+		"gray",
+		"lightgray"
+	};
+
 	std::chrono::system_clock::duration util::now() {
 		return std::chrono::system_clock::now().time_since_epoch();
 	}
@@ -37,5 +60,56 @@ namespace pingpong {
 	std::string util::rtrim(const std::string &str) {
 		std::string copy = str;
 		return rtrim(copy);
+	}
+
+	std::string util::irc2ansi(std::string str) {
+		// TODO: Unicode support
+		std::ostringstream out;
+		const size_t length = str.length();
+		str += "\0\0\0\0\0"; // 🤫
+
+		bool in_color = false, in_bold = false;
+
+		for (size_t i = 0; i < length; ++i) {
+			char ch = str[i];
+			if (ch == '\x03') {
+				if (!in_color) {
+					std::string next = str.substr(i + 1, 5);
+					std::string first = util::take_while(next, [](char c) { return std::isdigit(c); });
+					std::string second;
+					const size_t comma = next.find(',');
+					if (comma <= 2)
+						second = util::take_while(next.substr(comma + 1), [](char c) { return std::isdigit(c); });
+
+					if (first.empty())
+						continue;
+
+					long first_long = strtol(first.c_str(), nullptr, 10), second_long = -1;
+					if (!second.empty())
+						second_long = strtol(second.c_str(), nullptr, 10);
+
+					if (16 <= first_long || 16 <= second_long)
+						continue;
+
+					out << ansi::get_fg(ansi::get_color(irc_colors[first_long]));
+					if (-1 < second_long)
+						out << ansi::get_bg(ansi::get_color(irc_colors[second_long]));
+
+					i += first.length() + (comma <= 2? 1 + second.length() : 0);
+				} else {
+					out << ansi::reset_fg << ansi::reset_bg;
+				}
+
+				in_color = !in_color;
+			} else if (ch == '\x02') {
+				out << (in_bold? "\e[22m" : "\e[1m");
+				in_bold = !in_bold;
+			} else {
+				out << ch;
+			}
+		}
+
+		out << "\e[0m";
+		return out.str();
 	}
 }
