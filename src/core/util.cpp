b3a51c3841bf79b4a2ba6e7a867960793f3d5c55
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-30 22:01:24 -0700

    Added italics support

--- src/core/util.cpp
+++ src/core/util.cpp
@@ -68,7 +68,7 @@ namespace pingpong {
 		const size_t length = str.length();
 		str += "\0\0\0\0\0"; // 🤫
 
-		bool in_color = false, in_bold = false, in_underline = false;
+		bool in_color = false, in_bold = false, in_underline = false, in_italics = false;
 
 		for (size_t i = 0; i < length; ++i) {
 			char ch = str[i];
@@ -104,6 +104,9 @@ namespace pingpong {
 			} else if (ch == '\x02') {
 				out << (in_bold? "\e[22m" : "\e[1m");
 				in_bold = !in_bold;
+			} else if (ch == '\x1d') {
+				out << (in_italics? "\e[23m" : "\e[3m");
+				in_italics = !in_italics;
 			} else if (ch == '\x1f') {
 				out << (in_underline? "\e[24m" : "\e[4m");
 				in_underline = !in_underline;
