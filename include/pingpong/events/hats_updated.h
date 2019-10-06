3788e51620909e2efefa29aad6213809266548da
Kai Tamkun <ktamkun@ucsc.edu>
2019-10-06 16:10:54 -0700

    Fixed hats_updated

--- include/pingpong/events/hats_updated.h
+++ include/pingpong/events/hats_updated.h
@@ -12,8 +12,7 @@ namespace pingpong {
 		
 		hats_updated_event(const std::shared_ptr<user> &who_, const std::shared_ptr<channel> &chan_,
 		const hat_set &old_hats_, const hat_set &new_hats_):
-			user_event(who_, chan_, std::string(1, static_cast<char>(new_hat_))),
-			old_hats(old_hats_), new_hats(new_hats_) {}
+			user_event(who_, chan_, std::string(new_hats_)), old_hats(old_hats_), new_hats(new_hats_) {}
 	};
 }
 
