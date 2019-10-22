cae9eed2dba70578f6193687955d6a98d59dbd81
Kai Tamkun <ktamkun@ucsc.edu>
2019-10-21 17:00:23 -0700

    Fixed incorrect substring in nick_in_use dispatch

--- src/numerics/nick_in_use.cpp
+++ src/numerics/nick_in_use.cpp
@@ -1,9 +1,11 @@
 #include "pingpong/events/nick_in_use.h"
 #include "pingpong/messages/numeric.h"
 
+#include "lib/formicine/futil.h"
+
 namespace pingpong {
 	bool numeric_message::handle_nick_in_use(server *serv) {
-		events::dispatch<nick_in_use_event>(line.parameters.substr(line.parameters.find(' ')), serv);
+		events::dispatch<nick_in_use_event>(formicine::util::nth_word(line.parameters, 1), serv);
 		return true;
 	}
 }
