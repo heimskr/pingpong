8624596ed2060c11592298dab1593e803f6c4207
Kai Tamkun <ktamkun@ucsc.edu>
2021-07-31 14:51:03 -0700

    Fix messageless PART contents being the channel name instead of an empty string

--- src/messages/Part.cpp
+++ src/messages/Part.cpp
@@ -1,5 +1,3 @@
-#include <string>
-
 #include "pingpong/events/Part.h"
 #include "pingpong/events/NamesUpdated.h"
 
@@ -23,7 +21,15 @@ namespace PingPong {
 			Events::dispatch<NamesUpdatedEvent>(chan);
 		}
 
-		Events::dispatch<PartEvent>(who, chan, content);
+		// PARTs without a message look like "PART #foo". PARTS with a message look like "PART #foo :bar".
+		// Due to the lack of a colon, SourcedMessage's constructor interprets the channel of a messageless PART as its
+		// content, even though that's not the case. I could implement a workaround to prevent this behavior if virtual
+		// methods were callable from constructors, but because they're not, I have to read the Line here.
+		if (line.parameters.find(':') == std::string::npos)
+			Events::dispatch<PartEvent>(who, chan, "");
+		else
+			Events::dispatch<PartEvent>(who, chan, content);
+
 		return true;
 	}
 }
